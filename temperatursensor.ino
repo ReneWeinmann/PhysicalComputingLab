a20a07d4a246b752ed931f34dfbf3e9a678e839a
ReneWeinmann <Rene.Weinmann89@googlemail.com>
2016-05-12 21:33:10 +0200

    Add files via upload
    read temperture sensor LMT01 and plot the temperatur on the serial monitor.

---
+++ temperatursensor.ino
@@ -0,0 +1,24 @@
+const byte interruptPin = 2;
+const byte temp_on = 7;
+int temp_count;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(temp_on, OUTPUT);
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), tempcount, RISING );
+}
+
+void loop() {
+  digitalWrite(temp_on,HIGH);
+  delay(104);
+  digitalWrite(temp_on,LOW);
+  temp_count=((((float)temp_count/4096)*256)-50);
+  Serial.print(temp_count);
+  temp_count=0;
+  delay(100);
+}
+
+void tempcount() {
+  temp_count=temp_count+1;
+}
